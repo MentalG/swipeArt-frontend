62a40daaeda1021b1ce69bab2401e27ff4d1f5c1
MentalG <gromovthefirst@gmail.com>
2020-04-26 15:57:10 +0300

    fix redirect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import AppRoute from './AppRoute';
-// import PickSide from '../../pages/registration/PickSide';
-// import PickSide from '../../pages/registration/PickSide'
+import PickSide from '../../pages/PickSide'
 import Intro from '../../pages/intro';
 import store from '../../store';
 import 'antd/dist/antd.css';
@@ -13,7 +12,7 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <AppRoute path='/' component={Intro} exact />
-        {/* <AppRoute path='/registration/roll' component={PickSide} /> */}
+        <AppRoute path='/registration/roll' component={PickSide} />
       </Router>
     </Provider>
   );
