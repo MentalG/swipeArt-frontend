40fdd74e62ffca89e98fe73274981361436daa1d
MentalG <gromovthefirst@gmail.com>
2020-04-26 14:25:23 +0300

    add redirect

--- src/pages/intro/Intro.js
+++ src/pages/intro/Intro.js
@@ -2,11 +2,17 @@ import React, { useState } from 'react';
 import ReactSwipe from 'react-swipe';
 import Logo from '../../images/Logo.png';
 import Theatre from '../../images/Theatre.png';
+import Artist from '../../images/Artist.png';
+import Singer from '../../images/Singer.png';
+import Guitar from '../../images/Guitar.png';
 import Arrow from '../../components/ui-kit/icons/Arrow';
+import { useHistory } from "react-router-dom";
 import ProgressBar from '../../components/ProgressBar';
+
 import './styles.scss';
 
 const Intro = (props) => {
+  const history = useHistory();
   let slide = 0;
   let reactSwipeEl;
 
@@ -18,8 +24,13 @@ const Intro = (props) => {
           continuous: false,
           stopPropagation: true,
           transitionEnd: () => {
-            slide = reactSwipeEl.getPos();
+            // slide = reactSwipeEl.getPos();
+            // if(slide > 4) return history.push('/someRoute')
           },
+          callback: () => {
+            slide = reactSwipeEl.getPos();
+            if(slide > 4) return history.push('/registration/roll')
+          }
         }}
         ref={(el) => (reactSwipeEl = el)}
       >
@@ -53,7 +64,83 @@ const Intro = (props) => {
               <div className='dot'></div>
               <div className='dot'></div>
             </div>
+            <button className='TeaterSwipe'>Посвайпали</button>
           </div>
+        </div>
+        {/* ARTIST */}
+        <div>
+          <div className='artistWrapper'>
+            <img
+              className='artistImg'
+              src={Artist}
+              alt='Theatre'
+              width='255px'
+              height='255px'
+            />
+            <div className='artistText'>
+              Swipe – і ти продаєш
+              <br />
+              свою першу картину!
+            </div>
+            <div className='progressBarWrapper'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <button className='artistSwipe'>Посвайпали</button>
+          </div>
+        </div>
+        {/* ARTIST */}
+        {/* Singer */}
+        <div>
+          <div className='singerWrapper'>
+            <img
+              className='singerImg'
+              src={Singer}
+              alt='Theatre'
+              width='255px'
+              height='255px'
+            />
+            <div className='singerText'>
+              Swipe –
+              <br />і ти вже навчаєш співу!
+            </div>
+            <div className='progressBarWrapper'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <button className='singerSwipe'>Посвайпали</button>
+          </div>
+        </div>
+        {/* Singer */}
+        {/* guitar */}
+        <div>
+          <div className='guitarWrapper'>
+            <img
+              className='guitarImg'
+              src={Guitar}
+              alt='guitar'
+              width='255px'
+              height='255px'
+            />
+            <div className='guitarText'>
+              Swipe –
+              <br />і ти вже навчаєш співу!
+            </div>
+            <div className='progressBarWrapper'>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+              <div className='dot'></div>
+            </div>
+            <button className='guitarSwipe'>Посвайпали</button>
+          </div>
+        </div>
+        {/* guitar */}
+        <div className='redirect'>
         </div>
       </ReactSwipe>
       {/* <button onClick={() => reactSwipeEl.next()}>Next</button>
